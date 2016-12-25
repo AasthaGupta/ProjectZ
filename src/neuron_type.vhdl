c68e8fbe1a71a6c32688d96372416453ff3611c3
Aastha Gupta <aastha.gupta411@gmail.com>
2016-12-25 14:47:28 +0530

    network layer added

--- src/neuron_type.vhdl
+++ src/neuron_type.vhdl
@@ -1,3 +1,4 @@
 package neuron_types is
 	type neuron_in is array(0 to 8) of bit_vector(39 downto 0);
+	type neuron_out is array(0 to 8) of bit_vector(39 downto 0);
 end package;
---
+++ src/tests/test_network_layer.vhdl
@@ -0,0 +1,42 @@
+use work.neuron_types.all;
+
+entity test_network_layer is
+end entity;
+
+architecture test_network_layer_arch of test_network_layer is
+
+	component network_layer is
+        port(LAYER_INPUT : IN neuron_in;
+             LAYER_OUTPUT : OUT neuron_out;
+             START : IN bit;
+             CLOCK : IN bit;
+             READY : OUT bit);
+    end component;
+
+	signal network_inputs : neuron_in := ("0000000100000000000000000000000000000000",  -- 1.0
+    	                        	     "0000000010000000000000000000000000000000",  -- 0.5
+	                            	     "0000000100000000000000000000000000000000",  -- 1.0
+	                            	     "0000000010000000000000000000000000000000",  -- 0.5
+	                            	     "0000000100000000000000000000000000000000",  -- 1.0
+	                            	     "0000000010000000000000000000000000000000",  -- 0.5
+	                            	     "0000000100000000000000000000000000000000",  -- 1.0
+	                            	     "0000000010000000000000000000000000000000",  -- 0.5
+	                            	     "0000000100000000000000000000000000000000"); -- 1.0
+
+    signal network_out : neuron_out;
+	signal network_start, clk, network_ready : bit;
+
+	begin
+
+	NETWORK : network_layer port map(network_inputs, network_out, network_start, clk, network_ready);
+
+	clk <= not clk after 10 ns;
+
+	process
+	begin
+		network_start <= '1';
+		wait until network_ready = '1';
+
+	end process;
+
+end architecture;
